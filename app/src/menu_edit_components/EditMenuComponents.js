82ab6e06c86c017644bdfe3fbed287d624629aaa
samyu1204 <87253028+samyu1204@users.noreply.github.com>
2022-03-15 00:14:52 +1100

    Added a double click feature to update category data

--- app/src/menu_edit_components/EditMenuComponents.js
+++ app/src/menu_edit_components/EditMenuComponents.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
   View, 
   Text, 
   TouchableOpacity, 
   Platform, 
-  Dimensions,
-  Button
+  Pressable,
+  Modal
 } from "react-native";
+
 import global from "../global_information/global";
-import { editMenuStyles } from "../styles/EditMenuStyleSheet";
+import { editMenuStyles, modalStyles } from "../styles/EditMenuStyleSheet";
 import { Triangle } from "../styles/Shapes";
-import { EditElementPopUp } from "./EditMenuPopUps";
+import DoubleClick from "react-native-double-tap";
+import { Kohana } from "react-native-textinput-effects";
+import { Ionicons } from '@expo/vector-icons';
+
 
 // =============================================================================================================================
 //          =================================== Category Selection Buttons =======================================
@@ -18,42 +22,169 @@ import { EditElementPopUp } from "./EditMenuPopUps";
 export function CategoryDisplayUnselected(props) {
   return (
     <View>
-      <TouchableOpacity 
-        style={editMenuStyles.categoryButton}
-        onPress={() => {
+      <DoubleClick
+        singleTap={() => {
           props.selectCategory(props.categoryName);
         }}
-        
+        doubleTap={() => {
+          props.modalUpdate(true);
+          props.catIdGetter(props.categoryName)
+        }}
+        delay={200}
       >
-        <Text style={editMenuStyles.categoryButtonText}>
-          {global.categories[props.categoryName]['name']}
-        </Text>
-      </TouchableOpacity>
+        <View style={editMenuStyles.categoryButton}>
+          <Text style={editMenuStyles.categoryButtonText}>
+              {global.categories[props.categoryName]['name']}
+          </Text>
+        </View>
+      </DoubleClick>
     </View>
   );
 }
+
 
 export function CategoryDisplaySelected(props) {
   return (
     <View>
-      <TouchableOpacity 
-        style={editMenuStyles.selectedCategoryButton}
-        onPress={() => {
-          props.selectCategory(props.categoryName);
+      <DoubleClick
+        doubleTap={() => {
+          props.modalUpdate(true);
+          props.catIdGetter(props.categoryName)
         }}
+        delay={200}
       >
-        <View style={{
-          position: 'absolute',
-          left: Platform.OS === 'ios' ? '50%' : '59%',
-          top: '45%'
-        }}>
-          {<Triangle />}
+        <View 
+          style={editMenuStyles.selectedCategoryButton}
+          onPress={() => {
+            props.selectCategory(props.categoryName);
+          }}
+        >
+          <View style={{
+            position: 'absolute',
+            left: Platform.OS === 'ios' ? '50%' : '59%',
+            top: '45%'
+          }}>
+            {<Triangle />}
+          </View>
+
+          <Text style={editMenuStyles.categoryButtonText}>
+            {global.categories[props.categoryName]['name']}
+          </Text>
         </View>
-        <Text style={editMenuStyles.categoryButtonText}>
-          {global.categories[props.categoryName]['name']}
-        </Text>
-      </TouchableOpacity>
+      </DoubleClick>
     </View>
   );
 }
 
+/**
+ * Edit Category pop-up button:
+ * @param {*} props 
+ * @returns 
+ */
+export const EditCategoryPopUp = ({ props, refFunc, catRefFunc }) => {
+  const [modalVisible, setModalVisible] = useState(false)
+  const [catId, setCatId] = useState(null)
+  const [name, setName] = useState();
+
+  const categoryName = () => {
+    if (catId !== null) {
+      return global.categories[catId]['name'];
+    }
+    return;
+  }
+  
+  React.useEffect(() => {
+    refFunc.current = setModalVisible;
+    catRefFunc.current = setCatId;
+  }, []);
+
+  return (
+    <View style={modalStyles.centeredView}>
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={modalStyles.centeredView}>
+          <View style={modalStyles.editAdjustmentFieldModalView}>
+            
+            <View style={{
+              bottom: '30%'
+            }}>
+              <Text style={modalStyles.modalText}>Edit Category</Text>
+            </View>
+          
+            <Kohana
+                style={{ 
+                  backgroundColor: '#f9f5ed', 
+                  bottom: '10%',
+                  borderRadius: 15,
+                  marginRight: '2%'
+                }}
+                iconClass={Ionicons}
+                iconName={'pencil'}
+                iconColor={'#f4d29a'}
+                inputPadding={16}
+                labelStyle={{ color: '#BEB38B' }}
+                inputStyle={{ color: '#BEB38B' }}
+                labelContainerStyle={{ padding: 2 }}
+                iconContainerStyle={{ padding: 16 }}
+                useNativeDriver
+                onChangeText={(text) => setName(text)}
+                value={categoryName()}
+              />
+
+            {/* Edit adjustment field name */}
+            <Pressable
+              style={[modalStyles.editCategoryApplyButton]}
+              onPress={() => {
+                console.log('HELLO');
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={modalStyles.textStyle}>Apply</Text>
+            </Pressable>
+            
+            <View 
+            style={{
+              position: 'absolute',
+              backgroundColor: '#F8474A',
+              borderRadius: 10,
+              bottom: '25%',
+              left: '15%',
+              padding: 3
+            }}>
+              <Ionicons 
+                name='trash'
+                size={30}
+                color='white'
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+                />
+            </View>
+
+            <Ionicons 
+              style={{
+                color: 'red', 
+                position: 'absolute',
+                alignSelf: 'flex-end', 
+                marginRight: '1%', 
+                marginTop: '1%'
+              }} 
+              name='close' 
+              size={40} 
+              onPress={() => {
+                setModalVisible(!modalVisible);
+            }} /> 
+          </View>
+        </View>
+      </Modal>
+
+    </View>
+  );
+}
--- app/src/screens/EditMenuScreen.js
+++ app/src/screens/EditMenuScreen.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, ScrollView, Dimensions, Button, Text, TouchableOpacit
 import { 
   CategoryDisplayUnselected, 
   CategoryDisplaySelected,
+  EditCategoryPopUp
   } from "../menu_edit_components/EditMenuComponents";
   
 import { editMenuStyles } from "../styles/EditMenuStyleSheet";
@@ -21,6 +22,11 @@ function EditMenuScreen({ route, navigation }) {
   // State for category buttons
   const [categoryButtons, setCategoryButtons] = useState(null);
 
+  // Ref function to modal visibility for category: 
+  // This is for passing a functon from a lower level higher
+  const setModalVisibility = React.useRef(null);
+  const setTmpId = React.useRef(null);
+
   // Set items:
   const [items, setItems] = useState();
 
@@ -37,9 +43,21 @@ function EditMenuScreen({ route, navigation }) {
     const catIdList = Object.keys(global.menu_info[global.focusedMenu]['categories']);
     setCategoryButtons(catIdList.map(name => {
       if (currentCategory === name) {
-        return <CategoryDisplaySelected key={name} categoryName={name} selectCategory={selectNewCategory} />;
+        return <CategoryDisplaySelected 
+                  key={name} 
+                  categoryName={name} 
+                  selectCategory={selectNewCategory} 
+                  modalUpdate={setModalVisibility.current} 
+                  catIdGetter={setTmpId.current}
+                  />;
       } else {
-        return <CategoryDisplayUnselected key={name} categoryName={name} selectCategory={selectNewCategory} />;
+        return <CategoryDisplayUnselected 
+                  key={name} 
+                  categoryName={name} 
+                  selectCategory={selectNewCategory} 
+                  modalUpdate={setModalVisibility.current} 
+                  catIdGetter={setTmpId.current}
+                  />;
       }
     }))
   }
@@ -50,7 +68,15 @@ function EditMenuScreen({ route, navigation }) {
       //console.log(global.items)
       const itemIdList = Object.keys(global.categories[currentCategory]['items']);
       // Using id of item to construct components:
-      setItems(itemIdList.map(name => <ItemDisplay key={name} itemId={name} catId={currentCategory} updateScreen={renderItems}/>))
+      setItems(itemIdList.map(name => 
+        <ItemDisplay 
+          key={name} 
+          itemId={name} 
+          catId={currentCategory} 
+          updateScreen={renderItems}
+          />
+        )
+      )
     }
   }
 
@@ -78,16 +104,17 @@ function EditMenuScreen({ route, navigation }) {
         >
           {categoryButtons}
         </ScrollView>
-        
+
         <AddCategoryPopUp updateScreen={renderCategoryButtons} />
-      
+        <EditCategoryPopUp catRefFunc={setTmpId} refFunc ={setModalVisibility} />
       </View>
+      
       <View style={styles.itemsScrollView}>
         <ScrollView style={styles.itemsScroll}>
           {items}
         </ScrollView>  
       </View>
-
+      
       {/* Add item button: */}
       <View style={{
         position: 'absolute',
@@ -113,7 +140,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#E4F4E4',
     width: Dimensions.get('screen').width/6,
     left: Dimensions.get('screen').width/14,
-    height: (0.440 * Dimensions.get('screen').height),
+    height: (0.538 * Dimensions.get('screen').height),
     borderRadius: 30,
   },
   categoryScrollView: {
